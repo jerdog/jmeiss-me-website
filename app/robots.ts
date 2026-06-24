586146f82f4cc9a5a56b71af852fb146447a28b6
Jeremy Meiss <jeremy.meiss@gmail.com>
2026-06-23 22:50:23 -0500

    fix(seo): use bare hostname for robots.txt host directive

--- app/robots.ts
+++ app/robots.ts
@@ -11,6 +11,6 @@ export default function robots(): MetadataRoute.Robots {
       },
     ],
     sitemap: `${siteConfig.url}/sitemap.xml`,
-    host: siteConfig.url,
+    host: new URL(siteConfig.url).host,
   };
 }
